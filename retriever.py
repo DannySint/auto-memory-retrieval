ec64512e50ffd6b0c1ef6c9ec113e5ed8cc25f09
Danny <dannytsint@gmail.com>
2019-08-03 18:20:51 +0100

    get jpeg images

--- retriever.py
+++ retriever.py
@@ -1,1 +1,17 @@
 import praw
+
+reddit = praw.Reddit('retriever', user_agent='retriever user agent')
+
+def get_submissions(subreddit, amount):
+    subreddit = reddit.subreddit(subreddit)
+    return subreddit.hot(limit=amount)
+    
+def get_image_submissions(subreddit, amount):
+    images = []
+    submissions = get_submissions(subreddit, amount)
+
+    for submission in submissions:
+        if(str.endswith(submission.url, 'jpg')):
+            images.append(submission)
+            
+    return images
