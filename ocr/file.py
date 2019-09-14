385a8fd2a60eaf090973b6577358423836039fd4
Danny <dannytsint@gmail.com>
2019-09-14 01:56:26 +0100

    Document storage working

--- ocr/file.py
+++ ocr/file.py
@@ -3,28 +3,36 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 databases = myclient.list_database_names()
+#print("databases:",databases)
 database = myclient["database"]
+#print("database",database)
 collections = database.list_collection_names()
+#print("collections:",collections)
+debug = False
 
 
 def insert_single(subreddit, redditLink, url, title, transcription):
     transcriptions = database[subreddit]
-    if not alreadyExists:
+    if debug: print("alreadyExists:",bool(alreadyExists(subreddit, redditLink)))
+    if not bool(alreadyExists(subreddit, redditLink)):
+        if debug: print("Inserting...")
         dict = {"_id": redditLink, "imageUrl": url, "title": title, "transcription": transcription}
         transcriptions.insert_one(dict)
 
 def insert_multi(subreddit, dicts):
     multiDict = []
     transcriptions = database[subreddit]
-    for i in range(len(dicts)):
+    for i in range(dicts.Sized):
         multiDict.append(dicts[i])
     transcriptions.insert_many(multiDict)
 
 def alreadyExists(subreddit, redditLink):
     transcriptions = database[subreddit]
-    for i in range(len(transcriptions)):
-        if redditLink in transcriptions[i]._id:
-            return True
+    if debug: print("'Already Exists' function activation. Searching: ",transcriptions.name, "for ",redditLink)
+    #for i in range(transcriptions.count()):
+    result = transcriptions.find_one({ "_id": redditLink})
+    if (not (result == None)):
+        return True
     return False
 
 def search(subreddit, transcription):
--- ocr/main.py
+++ ocr/main.py
@@ -1,14 +1,15 @@
 import retriever
 import transcriber
-#import file
+import file
 
 subreddit = 'memes'
-posts = 5
+posts = 4
 
 images = retriever.get_image_submissions(subreddit, posts)
 
 for image in images:
     #x = transcriber.transcribe_image_from_path('data/cucumber.png')
     x = transcriber.transcribe_image_from_url(image.url)
-    append = "https://reddit.com/r/memes/comments/" + str(image)
-    print(append, image.title, image.url, x)
+    redditLink = "https://reddit.com/r/memes/comments/" + str(image)
+    #print(redditLink, image.url, image.title, x)
+    file.insert_single(subreddit, redditLink, image.url, image.title, x)
